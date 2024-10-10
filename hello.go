d891024b9d54a4178e31fa429b5746bb37900d5e
Andrew Aliaj <andyaliaj@gmail.com>
2024-10-10 12:34:17 -0400

    updates

--- hello.go
+++ hello.go
@@ -4,6 +4,16 @@ import "fmt"
 
 func main() {
     fmt.Println("Hello, World! This is my first code of Golang.")
+    
+    message := Hello()
+    fmt.Println(message)
+
+    // Declare and initialize the variable 'a'
+    a := 23
+    fmt.Println(a)
 }
 
-
+// Hello function that returns a string
+func Hello() string {
+    return "Hello, World! This is my first code of Golang."
+}

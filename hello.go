1fdcd984f7151ce893e32ebc4ba04dff03e422fc
Andrew Aliaj <andyaliaj@gmail.com>
2024-10-10 12:38:05 -0400

    guessing game

--- hello.go
+++ hello.go
@@ -1,19 +1,31 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math/rand"
+    "time"
+)
 
 func main() {
-    fmt.Println("Hello, World! This is my first code of Golang.")
-    
-    message := Hello()
-    fmt.Println(message)
+    randomSource := rand.New(rand.NewSource(time.Now().UnixNano()))
+    secretNumber := randomSource.Intn(100) + 1
+    var guess int
 
-    // Declare and initialize the variable 'a'
-    a := 23
-    fmt.Println(a)
-}
+    fmt.Println("Welcome to the Number Guessing Game!")
+    fmt.Println("I'm thinking of a number between 1 and 100. Can you guess it?")
 
-// Hello function that returns a string
-func Hello() string {
-    return "Hello, World! This is my first code of Golang."
+    for attempts := 0; attempts < 10; attempts++ {
+        fmt.Print("Enter your guess: ")
+        fmt.Scan(&guess)
+
+        if guess < secretNumber {
+            fmt.Println("Too low! Try again.")
+        } else if guess > secretNumber {
+            fmt.Println("Too high! Try again.")
+        } else {
+            fmt.Printf("Congratulations! You guessed the number %d!\n", secretNumber)
+            return
+        }
+    }
+    fmt.Printf("Out of attempts! The number was %d.\n", secretNumber)
 }
